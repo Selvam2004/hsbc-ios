5e8b407c3687b1a32bba9656238de8c245476a43
Selvam S <selvam.s@juspay.in>
2025-09-03 11:29:22 +0530

    2.2.4.6

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(name: "HyperSDK", url: "https://github.com/juspay/hypersdk-ios.git", .exact("2.2.4.5")),
+        .package(name: "HyperSDK", url: "https://github.com/juspay/hypersdk-ios.git", .exact("2.2.4.6")),
     ],
     targets: [
         .target(
